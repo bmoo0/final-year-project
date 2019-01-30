a56193f8100d08b414fad843795cb8c1b9bf8750
Ben Moore <bmoo@protonmail.com>
2019-01-30 15:14:55 +0000

    added material design libraries

--- bitcoinWallet/app/src/main/java/com/bitcoinwallet/MainActivity.kt
+++ bitcoinWallet/app/src/main/java/com/bitcoinwallet/MainActivity.kt
@@ -2,6 +2,9 @@ package com.bitcoinwallet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import org.bitcoinj.core.NetworkParameters
+import org.bitcoinj.wallet.Wallet
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,10 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val hasWallet: Boolean = getString(R.string.wallet_present).toBoolean()
+        val hasWallet: Boolean = resources.getBoolean(R.bool.wallet_present)
 
         if(!hasWallet) {
             // prompt to create or recover wallet
+            val wallet: Wallet =  Wallet(NetworkParameters.fromID(NetworkParameters.ID_TESTNET))
+            //val alertDialog:
         }
     }
 }
