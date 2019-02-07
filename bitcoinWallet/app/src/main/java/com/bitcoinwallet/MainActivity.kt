cf2279d5b6493732379b393ec0fdd3d72bac7edd
Ben Moore <bmoo@protonmail.com>
2019-02-07 17:24:28 +0000

    created button style

--- bitcoinWallet/app/src/main/java/com/bitcoinwallet/MainActivity.kt
+++ bitcoinWallet/app/src/main/java/com/bitcoinwallet/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.bitcoinwallet
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.view.Window
 import org.bitcoinj.core.NetworkParameters
 import org.bitcoinj.wallet.Wallet
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
