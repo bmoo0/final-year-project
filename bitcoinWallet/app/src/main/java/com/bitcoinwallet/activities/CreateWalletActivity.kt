901f4b5a770781c1763098f4f64c19ee8e7014d8
Ben Moore <benmoore422@gmail.com>
2019-02-12 19:30:50 +0000

    added create password screen and loading screen need to set thread

--- bitcoinWallet/app/src/main/java/com/bitcoinwallet/activities/CreateWalletActivity.kt
+++ bitcoinWallet/app/src/main/java/com/bitcoinwallet/activities/CreateWalletActivity.kt
@@ -3,16 +3,20 @@ package com.bitcoinwallet.activities
 import android.os.Bundle
 import android.app.Activity
 import android.content.DialogInterface
+import android.os.Handler
 import android.util.Log
+import android.view.View
 import com.bitcoinwallet.R
 import com.bitcoinwallet.utilities.InterfaceUtilities
 import com.bitcoinwallet.utilities.NetworkUtilities
+import kotlinx.android.synthetic.main.activity_create_wallet.*
 
 import org.bitcoinj.core.ECKey
 import org.bitcoinj.core.NetworkParameters
 import org.bitcoinj.core.listeners.DownloadProgressTracker
 import org.bitcoinj.kits.WalletAppKit
 import org.bitcoinj.utils.BriefLogFormatter
+import org.bitcoinj.utils.Threading
 import java.io.File
 import java.util.*
 
@@ -24,46 +28,84 @@ class CreateWalletActivity : Activity() {
         BriefLogFormatter.init()
         val file = File(filesDir, "wallet_file")
         //val progressDialog = ProgressDialog(this)
+        var walletAppKit: WalletAppKit
 
-        if (NetworkUtilities.isUsingMobile(this)) {
-            InterfaceUtilities.Companion.showAlertDialog(this, "Download Blockchain",
-                "Your mobile device is not on wifi, downloading the blockchain will be a large download," +
-                        "are you sure you want to continue?",
-                "Yes",
-                DialogInterface.OnClickListener { _, _ ->
-                    // continue
-                },
-                "No",
-                DialogInterface.OnClickListener { _, _ ->
-                    finish() // close activity
+        confirmCreateWalletButton.setOnClickListener {
+
+            if (NetworkUtilities.isUsingMobile(this)) {
+                InterfaceUtilities.Companion.showAlertDialog(this, "Download Blockchain",
+                    "Your mobile device is not on wifi, downloading the blockchain will be a large download," +
+                            "are you sure you want to continue?",
+                    "Yes",
+                    DialogInterface.OnClickListener { _, _ ->
+                        // continue
+                    },
+                    "No",
+                    DialogInterface.OnClickListener { _, _ ->
+                        finish() // close activity
+                    }
+                )
+            }
+
+
+            if (password_input.text.toString() != password_input_confirm.text.toString()) {
+                errorMessage.visibility = View.VISIBLE
+            } else {
+                errorMessage.visibility = View.GONE
+                val walletPassword = password_input.text.toString()
+
+                walletAppKit = object : WalletAppKit(
+                    NetworkParameters.fromID(NetworkParameters.ID_TESTNET), file,
+                    "Test Wallet Name"
+                ) {
+                    override fun onSetupCompleted() {
+                        if (wallet().importedKeys.size > 1) {
+                            wallet().importKey(ECKey())
+                        }
+                        wallet().allowSpendingUnconfirmedTransactions()
+
+                        Log.d("BTC WALLET", "My address = " + wallet().freshReceiveAddress())
+                    }
                 }
-            )
-        }
 
-        val walletAppKit = object : WalletAppKit(
-            NetworkParameters.fromID(NetworkParameters.ID_TESTNET), file,
-            "Test Wallet Name"
-        ) {
-            override fun onSetupCompleted() {
-                if (wallet().importedKeys.size > 1) {
-                    wallet().importKey(ECKey())
-                }
-                wallet().allowSpendingUnconfirmedTransactions()
+                changeToLoadingScreen()
+                //downloading_blockchain_progress_bar.isAnimating = true
 
-                Log.d("BTC WALLET", "My address = " + wallet().freshReceiveAddress())
+                walletAppKit.setDownloadListener(object : DownloadProgressTracker() {
+                    override fun progress(pct: Double, blocksSoFar: Int, date: Date?) {
+                        super.progress(pct, blocksSoFar, date)
+                        val percentage: Int = pct.toInt()
+                        downloading_blockchain_progress_bar.setProgress(percentage, true)
+                        downloading_blockchain_percentage.text = percentage.toString() + "%"
+                    }
+
+                    override fun doneDownload() {
+                        super.doneDownload()
+                        downloading_blockchain_progress_bar.setProgress(100,true)
+                        walletAppKit.wallet().encrypt(walletPassword)
+                    }
+                })
+
+                walletAppKit.setBlockingStartup(false)
+                walletAppKit.startAsync()
             }
         }
-
-        walletAppKit.setDownloadListener(object : DownloadProgressTracker() {
-            override fun progress(pct: Double, blocksSoFar: Int, date: Date?) {
-                super.progress(pct, blocksSoFar, date)
-                val percentage: Int = pct.toInt()
-            }
-        })
-
-        //walletAppKit.wallet().encrypt()
-
-        //walletAppKit.setBlockingStartup(false)
-        //walletAppKit.startAsync()
     }
+
+    fun changeToLoadingScreen() {
+        if (create_password_form.visibility == View.VISIBLE) {
+            create_password_form.visibility = View.GONE
+            loading_screen.visibility = View.VISIBLE
+        } else {
+            create_password_form.visibility = View.VISIBLE
+            loading_screen.visibility = View.GONE
+        }
+    }
+
+    /*
+    fun setBTCSDKThread() {
+        var handler = Handler()
+        Threading.USER_THREAD =
+    }
+    */
 }
